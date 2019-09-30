dba845ef6532321e2ebf050dd0c4400083273c57
osantana19 <55363326+osantana19@users.noreply.github.com>
2019-09-29 22:13:28 -0400

    Update CreateGroup.sh

--- CreateGroup.sh
+++ CreateGroup.sh
@@ -4,4 +4,6 @@ echo "Write group name you'd like to create"
 read groupname
 sudo groupadd ${groupname}
 sudo mkdir ./Users/${groupname}
+sudo mkdir ./Users/${groupname}/Policy
+sudo chmod 744 ./Users/${groupname}/Policy
 echo 'Group name' ${groupname} 'has been created'
